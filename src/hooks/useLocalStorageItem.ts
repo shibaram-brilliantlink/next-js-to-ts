bd7a28e82271cd9c54931674a344d46c1cfdd9ad
shibaramdas-git <shibaram.daseng@gamil.com>
2024-12-30 17:07:08 +0530

    useLocalStorage converted with

--- src/hooks/useLocalStorageItem.ts
+++ src/hooks/useLocalStorageItem.ts
@@ -3,6 +3,9 @@ import { useCallback, useEffect, useState } from "react";
 import { tryParse } from "../libs/tryParse";
 
 /**
+ *  * Custom hook to manage a localStorage item with React state synchronization.
+ *
+ *
  * @param {string} key Key to store in localStorage.
  * @param {*} blankValue Blank value to use before mounting.
  * @param {*} [initialValue] Value to fill in if not already stored.
@@ -11,15 +14,17 @@ import { tryParse } from "../libs/tryParse";
  * This does not attempt to retrieve from localStorage immediately,
  * as doing so would cause a React SSR/client hydration mismatch.
  */
-export function useLocalStorageItem(
-  key,
-  blankValue,
-  initialValue = blankValue
-) {
+
+export function useLocalStorageItem<T>(
+  key: string,
+  blankValue: T,
+  initialValue: T = blankValue
+): [T, (value: T) => void] {
   const [item, setItem] = useState(blankValue);
 
   useEffect(() => {
-    setItem(tryParse(localStorage.getItem(key)) ?? initialValue);
+    // TO DO: FIX ME LATER 🤔 USED ! BELOW ;
+    setItem(tryParse(localStorage.getItem(key)!) ?? initialValue);
   }, [initialValue, key]);
 
   const updateItem = useCallback(
